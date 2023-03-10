b7e7c724b939cef06ce7e635def4cb2d51c42d0a
Davide Bronzini <davide_bronzini@hotmail.com>
2023-03-10 14:38:57 +0100

    all

--- src/Http/Controllers/CrudAddressController.php
+++ src/Http/Controllers/CrudAddressController.php
@@ -136,18 +136,6 @@ class CrudAddressController extends CRUD
 
 
 
-     /**
-      * START CREATE PARAMETERS AND METHODS
-      **/
-
-    // public function beforeRenderCreate()
-    // {
-    //     $this->modelInstance->agent_id = session('agent')->getKey();
-    // }
-
-     /**
-      * STOP CREATE PARAMETERS AND METHODS
-      **/
 
 }
 
