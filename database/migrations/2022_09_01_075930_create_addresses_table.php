b5eba60cefc0c6eca433fa215ec0e0352ff5e97c
Davide Bronzini <davide_bronzini@hotmail.com>
2023-04-14 11:01:55 +0200

    all

--- database/migrations/2022_09_01_075930_create_addresses_table.php
+++ database/migrations/2022_09_01_075930_create_addresses_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('addresses', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
 
-            $table->nullableMorphs('addressable');
+            $table->string('addressable_type')->nullable();
+            $table->uuid('addressable_id')->nullable();
 
             $table->string('type')->nullable();
 
--- src/Models/Address.php
+++ src/Models/Address.php
@@ -3,9 +3,15 @@
 namespace IlBronza\Addresses\Models;
 
 use IlBronza\CRUD\Models\BaseModel;
+use IlBronza\CRUD\Traits\Model\CRUDUseUuidTrait;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Address extends BaseModel
 {
+	use CRUDUseUuidTrait;
 
-
+	public function addressable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
